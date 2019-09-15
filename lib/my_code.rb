18cd6ec0e76eb2ddad6fc02b4ec65e09af86d315
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-15 19:20:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,7 +24,7 @@ end
 def map_to_square(source_array) do
   counter = 0 
   while counter < source_array.length do
-    source_array[counter] **= 2 
+    source_array[counter] *= source_array[counter] 
     counter += 1
   end
   source_array
