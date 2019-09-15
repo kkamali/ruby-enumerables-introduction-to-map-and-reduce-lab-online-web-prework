62009d9ff2b16bebd92df9327889cd449319427a
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-15 19:18:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,3 +11,11 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  counter = 0 
+  while counter < source_array.length do
+    source_array[counter] *= 2
+    counter += 1
+  end
+end
