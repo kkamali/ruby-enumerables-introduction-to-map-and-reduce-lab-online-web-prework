68d39725f781b86bbae982217c36bd5117c99585
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-15 19:28:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,6 +45,7 @@ end
 
 def reduce_to_total(source_array, starting_point = 0)
   total = starting_point
+  counter = 0
   while counter < source_array.length do
     total += source_array[counter]
     counter += 1
